f45024d0120d36e3a69b30e2a8ec6d7a0bda071b
Joelroys637 <97875782+Joelroys637@users.noreply.github.com>
2024-12-25 14:21:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 import adout as ab
 
-adsense_html = """
+st.markdown("""
 <head>
     <meta name="google-adsense-account" content="ca-pub-7596984091672717">
     <title>Example Streamlit Page</title>
